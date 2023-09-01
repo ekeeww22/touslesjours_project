ec7e4193b5b2ed97993354a3c742a393d4f75ecf
kim <gangssu0@gmail.com>
2023-09-02 00:54:20 +0900

    main 배너 작업

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!-- footer -->
+<footer>
+    <div class="f_top">
+        <a href="#"><img src="./images/f_logo_cjfood.png" alt="cj푸드빌"></a>
+        <a href="#"><img src="./images/f_logo_vips.png" alt="vips"></a>
+        <a href="#"><img src="./images/f_logo_the_place.png" alt="더플레이스"></a>
+        <a href="#"><img src="./images/f_logo_noodle.png" alt="제일제면소"></a>
+        <a href="#"><img src="./images/f_logo_n_seoul_tower.png" alt="n서울타워"></a>
+    </div>
+    <div class="f_btm">
+        <ul class="fnb">
+            <li><a href="#">회사소개</a></li>
+            <li><a href="#">이용약관</a></li>
+            <li><a href="#">개인정보처리방침</a></li>
+            <li><a href="#">위치기반서비스 이용약관</a></li>
+            <li><a href="#">이메일무단수집거부</a></li>
+            <li><a href="#">기사 채용안내</a></li>
+            <li><a href="#">창업안내</a></li>
+            <li><a href="#">윤리신고센터</a></li>
+            <li><a href="#">법적고지</a></li>
+            <li><a href="#">사이트맵</a></li>
+        </ul>
+        <p>고객센터 : 1577 - 0700 / 9시 - 18시 (점심 12시 - 13시) ※ 주말, 공휴일 제외</p>
+        <address>상호명 : CJ푸드빌(주)   주소 : (우) 04555 서울시 중구 마른내로34(초동 106-9번지) KT&G을지로타워 3,9-11층 CJ푸드빌(주)<br>
+            통신판매업종신고증 제 2011-서울중구-0771호<a href="#" class="company_info">사업자정보확인</a>호스팅제공자 : CJ올리브네트웍스(주)<br>
+            사업자등록번호 : 312-81-42519   개인정보보호책임자 : 김재완   대표이사 : 김찬호   <a href="help_master@cj.net" class="mail">대표 이메일 : help_master@cj.net</a></address>
+        <span>COPYRIGHT 2023©CJ Foodville ALL RIGHT RESERVED.</span>
+    </div>
+</footer><!-- footer end -->
---
+++ header.php
@@ -0,0 +1,140 @@
+<!-- header -->
+<header>
+    <div class="h_top">
+        <div class="header_sns">
+            <a href="#"><img src="./images/icon/facebook_w.png" alt="facebook"></a>
+            <a href="#"><img src="images/icon/twitter_w.png" alt="twitter"></a>
+            <a href="#"><img src="./images/icon/instagram_w.png" alt="instagram"></a>
+        </div>
+        <h1><a href="./index.html"><img src="./images/logo_w.png" alt="뚜레쥬르"></a></h1>
+        <div class="h_top_r">
+            <a href="./login.html">LOGIN</a>
+            <form action="#" method="get" id="searchFrm">
+                <fieldset>
+                    <legend class="skip">검색양식</legend>
+                    <input type="text" id="searchTxt" placeholder="상품명을 입력해주세요">
+                    <button type="submit" id="searchBtn"><img src="./images/icon/search_w.png" alt="검색"></button>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+    <nav>
+        <ul class="gnb">
+            <li><a href="#">브랜드 스토리</a>
+                <ul class="sub">
+                    <li><a href="./product.html">브랜드 스토리</a></li>
+                    <li><a href="./product.html">뚜레쥬르 광고</a></li>
+                    <li><a href="./product.html">뚜레쥬르 재료 이야기</a></li>
+                    <li><a href="./product.html">글로벌 뚜레쥬르 </a></li>
+                    <li><a href="./product.html">뚜레쥬르 라뜰리에</a></li>
+                    <li><a href="./product.html">뚜레쥬르 착한빵</a></li>
+                    <li><a href="./product.html">뉴스 & 공지사항</a></li>
+                </ul>
+            </li>
+            <li><a href="#">새로워진 뚜레쥬르</a>
+                <ul class="sub">
+                    <li><a href="./product.html">New 뚜레쥬르 살펴보기</a></li>
+                    <li><a href="./product.html">건강한 데일리 베이커리</a></li>
+                </ul>
+            </li>
+            <li><a href="#">제품 안내</a>
+                <ul class="sub">
+                    <li><a href="./product.html">신제품</a></li>
+                    <li><a href="./product.html">빵</a></li>
+                    <li><a href="./product.html">케이크</a></li>
+                    <li><a href="./product.html">델리</a></li>
+                    <li><a href="./product.html">선물</a></li>
+                    <li><a href="./product.html">디저트&스낵</a></li>
+                    <li><a href="./product.html">음료</a></li>
+                </ul>
+            </li>
+            <li><a href="#">이벤트&멤버십</a>
+                <ul class="sub">
+                    <li><a href="./product.html">진행중인 이벤트</a></li>
+                    <li><a href="./product.html">이벤트 당첨자</a></li>
+                    <li><a href="./product.html">CJ ONE 멤버십</a></li>
+                    <li><a href="./product.html">제휴 혜택</a></li>
+                </ul>
+            </li>
+            <li><a href="#">앱&온라인 쇼핑몰</a>
+                <ul class="sub">
+                    <li><a href="./product.html">뚜레쥬르 앱</a></li>
+                    <li><a href="./product.html">홈페이지 전용 예약 배송</a></li>
+                    <li><a href="#">꽃 & 케이크 택배 배송</a></li>
+                    <li><a href="#">대량 구매 문의</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="nav_bg"></div>
+    </nav>
+    <!-- mobile open nav -->
+    <div class="m_open_nav">
+        <h1><a href="#"><img src="./images/logo_w.png" alt=""></a></h1>
+        <div class="right">
+            <a href="#" class="login"><img src="./images/icon/login_w.png" alt=""></a>
+            <a href="#" class="search_shop"><img src="./images/icon/map_w.png" alt=""></a>
+            <a href="#" class="m_nav"><span></span><span></span><span></span></a>
+        </div>
+    </div>
+    <div class="m_open_menu">
+        <button type="button" class="close_btn"><img src="./images/icon/close_btn.png" alt=""></button>
+        <form action="#" method="get" id="open_searchFrm">
+            <fieldset>
+                <legend class="skip">검색양식</legend>
+                <input type="text" id="searchTxt" placeholder="제품명을 입력해주세요">
+                <button type="submit" id="searchBtn"><img src="./images/icon/search_b.png" alt="검색"></button>
+            </fieldset>
+        </form>
+        <ul class="gnb">
+            <li><a href="#">브랜드 스토리</a>
+                <ul class="sub">
+                    <li><a href="#">브랜드 스토리</a></li>
+                    <li><a href="#">뚜레쥬르 광고</a></li>
+                    <li><a href="#">뚜레쥬르 재료 이야기</a></li>
+                    <li><a href="#">글로벌 뚜레쥬르 </a></li>
+                    <li><a href="#">뚜레쥬르 라뜰리에</a></li>
+                    <li><a href="#">뚜레쥬르 착한빵</a></li>
+                    <li><a href="#">뉴스 & 공지사항</a></li>
+                </ul>
+            </li>
+            <li><a href="#">새로워진 뚜레쥬르</a>
+                <ul class="sub">
+                    <li><a href="#">New 뚜레쥬르 살펴보기</a></li>
+                    <li><a href="#">건강한 데일리 베이커리</a></li>
+                </ul>
+            </li>
+            <li><a href="#">제품 안내</a>
+                <ul class="sub">
+                    <li><a href="#">신제품</a></li>
+                    <li><a href="#">빵</a></li>
+                    <li><a href="#">케이크</a></li>
+                    <li><a href="#">델리</a></li>
+                    <li><a href="#">선물</a></li>
+                    <li><a href="#">디저트&스낵</a></li>
+                    <li><a href="#">음료</a></li>
+                </ul>
+            </li>
+            <li><a href="#">이벤트&멤버십</a>
+                <ul class="sub">
+                    <li><a href="#">진행중인 이벤트</a></li>
+                    <li><a href="#">이벤트 당첨자</a></li>
+                    <li><a href="#">CJ ONE 멤버십</a></li>
+                    <li><a href="#">제휴 혜택</a></li>
+                </ul>
+            </li>
+            <li><a href="#">앱&온라인 쇼핑몰</a>
+                <ul class="sub">
+                    <li><a href="#">뚜레쥬르 앱</a></li>
+                    <li><a href="#">홈페이지 전용 예약 배송</a></li>
+                    <li><a href="#">꽃 & 케이크 택배 배송</a></li>
+                    <li><a href="#">대량 구매 문의</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="open_menu_sns">
+            <a href="#"><img src="./images/icon/facebook_b.png" alt="페이스북"></a>
+            <a href="#"><img src="./images/icon/twitter_b.png" alt="트위터"></a>
+            <a href="#"><img src="./images/icon/instagram.png" alt="인스타그램"></a>
+        </div>
+    </div>
+</header><!-- header end -->
