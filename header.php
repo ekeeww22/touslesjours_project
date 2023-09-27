f32c979385b25b1b7efa740605df39eff85d1c3e
eunha <gangssu0@gmail.com>
2023-09-27 14:48:15 +0900

    final

--- header.php
+++ header.php
@@ -60,8 +60,8 @@
                 <ul class="sub">
                     <li><a href="./product.html">뚜레쥬르 앱</a></li>
                     <li><a href="./product.html">홈페이지 전용 예약 배송</a></li>
-                    <li><a href="#">꽃 & 케이크 택배 배송</a></li>
-                    <li><a href="#">대량 구매 문의</a></li>
+                    <li><a href="./product.html">꽃 & 케이크 택배 배송</a></li>
+                    <li><a href="./product.html">대량 구매 문의</a></li>
                 </ul>
             </li>
         </ul>
@@ -69,10 +69,10 @@
     </nav>
     <!-- mobile open nav -->
     <div class="m_open_nav">
-        <h1><a href="#"><img src="./images/logo_w.png" alt=""></a></h1>
+        <h1><a href="./index.html"><img src="./images/logo_w.png" alt=""></a></h1>
         <div class="right">
-            <a href="#" class="login"><img src="./images/icon/login_w.png" alt=""></a>
-            <a href="#" class="search_shop"><img src="./images/icon/map_w.png" alt=""></a>
+            <a href="./login.html" class="login"><img src="./images/icon/login_w.png" alt=""></a>
+            <a href="./map.html" class="search_shop"><img src="./images/icon/map_w.png" alt=""></a>
             <a href="#" class="m_nav"><span></span><span></span><span></span></a>
         </div>
     </div>
@@ -88,46 +88,46 @@
         <ul class="gnb">
             <li><a href="#">브랜드 스토리</a>
                 <ul class="sub">
-                    <li><a href="#">브랜드 스토리</a></li>
-                    <li><a href="#">뚜레쥬르 광고</a></li>
-                    <li><a href="#">뚜레쥬르 재료 이야기</a></li>
-                    <li><a href="#">글로벌 뚜레쥬르 </a></li>
-                    <li><a href="#">뚜레쥬르 라뜰리에</a></li>
-                    <li><a href="#">뚜레쥬르 착한빵</a></li>
-                    <li><a href="#">뉴스 & 공지사항</a></li>
+                    <li><a href="./product.html">브랜드 스토리</a></li>
+                    <li><a href="./product.html">뚜레쥬르 광고</a></li>
+                    <li><a href="./product.html">뚜레쥬르 재료 이야기</a></li>
+                    <li><a href="./product.html">글로벌 뚜레쥬르 </a></li>
+                    <li><a href="./product.html">뚜레쥬르 라뜰리에</a></li>
+                    <li><a href="./product.html">뚜레쥬르 착한빵</a></li>
+                    <li><a href="./product.html">뉴스 & 공지사항</a></li>
                 </ul>
             </li>
             <li><a href="#">새로워진 뚜레쥬르</a>
                 <ul class="sub">
-                    <li><a href="#">New 뚜레쥬르 살펴보기</a></li>
-                    <li><a href="#">건강한 데일리 베이커리</a></li>
+                    <li><a href="./product.html">New 뚜레쥬르 살펴보기</a></li>
+                    <li><a href="./product.html">건강한 데일리 베이커리</a></li>
                 </ul>
             </li>
             <li><a href="#">제품 안내</a>
                 <ul class="sub">
-                    <li><a href="#">신제품</a></li>
-                    <li><a href="#">빵</a></li>
-                    <li><a href="#">케이크</a></li>
-                    <li><a href="#">델리</a></li>
-                    <li><a href="#">선물</a></li>
-                    <li><a href="#">디저트&스낵</a></li>
-                    <li><a href="#">음료</a></li>
+                    <li><a href="./product.html">신제품</a></li>
+                    <li><a href="./product.html">빵</a></li>
+                    <li><a href="./product.html">케이크</a></li>
+                    <li><a href="./product.html">델리</a></li>
+                    <li><a href="./product.html">선물</a></li>
+                    <li><a href="./product.html">디저트&스낵</a></li>
+                    <li><a href="./product.html">음료</a></li>
                 </ul>
             </li>
             <li><a href="#">이벤트&멤버십</a>
                 <ul class="sub">
-                    <li><a href="#">진행중인 이벤트</a></li>
-                    <li><a href="#">이벤트 당첨자</a></li>
-                    <li><a href="#">CJ ONE 멤버십</a></li>
-                    <li><a href="#">제휴 혜택</a></li>
+                    <li><a href="./product.html">진행중인 이벤트</a></li>
+                    <li><a href="./product.html">이벤트 당첨자</a></li>
+                    <li><a href="./product.html">CJ ONE 멤버십</a></li>
+                    <li><a href="./product.html">제휴 혜택</a></li>
                 </ul>
             </li>
             <li><a href="#">앱&온라인 쇼핑몰</a>
                 <ul class="sub">
-                    <li><a href="#">뚜레쥬르 앱</a></li>
-                    <li><a href="#">홈페이지 전용 예약 배송</a></li>
-                    <li><a href="#">꽃 & 케이크 택배 배송</a></li>
-                    <li><a href="#">대량 구매 문의</a></li>
+                    <li><a href="./service.html">뚜레쥬르 앱</a></li>
+                    <li><a href="./service.html">홈페이지 전용 예약 배송</a></li>
+                    <li><a href="./service.html">꽃 & 케이크 택배 배송</a></li>
+                    <li><a href="./service.html">대량 구매 문의</a></li>
                 </ul>
             </li>
         </ul>
